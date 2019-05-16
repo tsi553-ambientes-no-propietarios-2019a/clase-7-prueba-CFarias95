70002550aa910e1a198474396996f8e8d29352fd
carlos.farias@epn.edu.ec <carlos.farias@epn.edu.ec>
2019-05-15 22:48:04 -0500

    Prueba 1

--- PHP/salir.php
+++ PHP/salir.php
@@ -2,7 +2,7 @@
 session_start();
 if(isset($_SESSION['user'])){
     session_destroy();
-    header('Location: ../index.phperror_message=ha cerrado sesion corectamente');
+    header('Location: ../index.php?error_message=ha cerrado sesion corectamente');
     exit();
 
 }
